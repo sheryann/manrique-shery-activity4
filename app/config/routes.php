d2e1a47f4314b4d731c0d0a981b750a17546a032
Shery Ann Manrique <sheryannmanrique37@gmail.com>
2026-09-06 12:06:17 +0800

    Update routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,6 +44,6 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-$router->get('/', 'Welcome::index');
+$router->get('/', 'UsersController::index');
 
 $router->get('/users', 'UsersController::index');
